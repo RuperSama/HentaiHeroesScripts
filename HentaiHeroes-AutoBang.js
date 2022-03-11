c4e0888ba4ee75f8d42a6b9f2d896c4c144c246a
RuperSama <rupersama@gmail.com>
2022-03-11 10:37:17 -0300

    Update HentaiHeroes-AutoBang.js

--- HentaiHeroes-AutoBang.js
+++ HentaiHeroes-AutoBang.js
@@ -1,24 +1,30 @@
 // ==UserScript==
 // @name         HH AutoBang
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  try to take over the world!
-// @author       RuperSama
+// @author       RuperSama, cossname
 // @match        http*://*.haremheroes.com/*
 // @match        http*://*.hentaiheroes.com/*
 // @match        http*://*.gayharem.com/*
 // @match        http*://*.comixharem.com/*
 // @match        http*://*.hornyheroes.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=comixharem.com
+// @updateURL    https://github.com/RuperSama/HentaiHeroesScripts/blob/main/HentaiHeroes-AutoBang.js
+// @downloadURL  https://github.com/RuperSama/HentaiHeroesScripts/blob/main/HentaiHeroes-AutoBang.js
 // @grant        none
 // ==/UserScript==
 
 function startScript(){
     try{
-        if(document.getElementById("start-bang-button") != null)
+        if(document.getElementById("new-battle-skip-btn") != null){
+            document.getElementById("new-battle-skip-btn").click()
+        }
+        if(document.getElementById("start-bang-button") != null){
             document.getElementById("start-bang-button").click()
-        if(document.getElementsByClassName("blue_button_L").length > 0)
+            }
+        if(document.getElementsByClassName("blue_button_L").length > 0){
             document.getElementsByClassName("blue_button_L")[0].click();
+            }
     }
     catch(e){
         var a = e;
