7761567cec7519e9bfd03d1e8a2a7094f4a072d3
YotoTheOne <74134758+YotoTheOne@users.noreply.github.com>
2024-01-14 18:03:39 +0100

    Update HentaiHeroes-AutoBang.js
    Prevents the script from running outside of Boss Bang tab and when the event is completed.

--- HentaiHeroes-AutoBang.js
+++ HentaiHeroes-AutoBang.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         HH AutoBang
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
-// @author       RuperSama, cossname
+// @author       RuperSama, cossname, YotoTheOne
 // @match        http*://*.haremheroes.com/*
 // @match        http*://*.hentaiheroes.com/*
 // @match        http*://*.gayharem.com/*
@@ -15,16 +15,41 @@
 // ==/UserScript==
 
 function startScript(){
+    // attempting to stop the script when boss bang event is completed, not properly tested :
+    let url = window.location.href.split("?");
+    if(url.length != 2){
+        //console.log("Not on Boss Bang page.");
+        return;
+    }
+    else if(url[1].slice(4, 19) != "boss_bang_event"){
+        //console.log("Not on Boss Bang page.");
+        return;
+    }
+    if(document.getElementsByClassName("boss-bang-event-info completed-event") != null){
+        //console.log("Boss Bang event completed.");
+        return;
+    }
     try{
         if(document.getElementById("new-battle-skip-btn") != null){
-            document.getElementById("new-battle-skip-btn").click()
+            document.getElementById("new-battle-skip-btn").click();
+            //console.log("new-battle-skip-btn clicked");
         }
         if(document.getElementById("start-bang-button") != null){
-            document.getElementById("start-bang-button").click()
+            document.getElementById("start-bang-button").click();
+            //console.log("start-bang-button clicked");
+        }
+        let btn_list = document.getElementsByClassName("blue_button_L");
+        let lg = btn_list.length;
+        if(lg > 1){
+            for(let i = 0; i < lg; i++){
+                //console.log(btn_list[i].id)
+                if(btn_list[i].id == ""){
+                    btn_list[i].click();
+                    //console.log("blue_button_L clicked");
+                    break;
+                }
             }
-        if(document.getElementsByClassName("blue_button_L").length > 0){
-            document.getElementsByClassName("blue_button_L")[0].click();
-            }
+        }
     }
     catch(e){
         var a = e;
@@ -33,10 +58,6 @@ function startScript(){
         setTimeout(startScript, 1000);
     }
 }
-
-
-
-
 
 
 window.onload = startScript();
